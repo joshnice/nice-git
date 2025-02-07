2aa7096b64b903611861b02d150f40a2cd7e8f32
joshnice <joshliamnice@gmail.com>
2025-02-07 18:59:46 +0000

    Made design a lot more responsive

--- src/client/pages/home-page.tsx
+++ src/client/pages/home-page.tsx
@@ -14,21 +14,23 @@ import { RepoTitleComponent } from "./repo-title-component";
 
 export function HomePage() {
 	return (
-		<div className="h-full w-full p-5 flex flex-col gap-2 overflow-hidden">
+		<div className="h-screen w-screen p-5 flex flex-col gap-2 overflow-hidden">
 			<RepoSelectorComponent />
-			<div className="flex gap-4 flex-grow">
+			<div className="flex gap-4 flex-grow flex-shrink h-[calc(100%-82px)]">
 				<div className="w-1/3">
 					<BranchesSelectorComponent />
 				</div>
 				<div className="flex flex-col gap-3 w-2/3">
-					<RepoTitleComponent />
-					<div className="h-1/2 overflow-hidden">
+					<div className="flex-shrink-0 flex-grow-0">
+						<RepoTitleComponent />
+					</div>
+					<div className="overflow-hidden basis-1/2 flex-grow flex-shrink">
 						<BranchCommitsComponent />
 					</div>
-					<div className="h-1/2 overflow-hidden">
+					<div className="overflow-hidden basis-1/2 flex-grow flex-shrink">
 						<BranchChangesComponent />
 					</div>
-					<div>
+					<div className="flex-shrink-0 flex-grow-0">
 						<CommitMessageComponent />
 					</div>
 				</div>
